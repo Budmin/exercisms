e46109400d06c4217cca882b60f03b14ebdef041
budmin <garvin.drew@gmail.com>
2021-03-18 17:43:27 -0400

    3/18/2021

--- c/darts/src/darts.c
+++ c/darts/src/darts.c
@@ -1,8 +1,26 @@
+#include <stdio.h>
 #include "darts.h"
 
 
-int points_earned(float x, float y){
+uint8_t score(coordinate_t position){
 
-	
+    float distance_to_center = sqrt( (position[0] * position[0]) + (position[1] * position[1]) );
+
+    uint8_t output = 0;
+
+    // inner circle
+    if(distance_to_center <= 1){
+        output = 10;
+    }
+    // middle circle
+    else if(distance_to_center <= 5){
+        output = 5;
+    }
+    else if(distance_to_center <= 10){
+        output = 1;
+    }
+
+    return output;
 
 }
+
--- c/darts/src/darts.h
+++ c/darts/src/darts.h
@@ -1,4 +1,13 @@
 #ifndef DARTS_H
 #define DARTS_H
 
+#include <stdint.h>
+#include <math.h>
+
+typedef float coordinate_t[2];
+
+uint8_t score(coordinate_t position);
+
+// coordinate_t landing_position = { -9.0F, 9.0F };
+
 #endif
