a3d5752390a2a4fef181ab4da3acc0bc6e95181b
Budmin <garvin.drew@gmail.com>
2021-04-12 20:08:43 -0400

    4/12/2021

--- cpp/armstrong-numbers/build/CMakeFiles/test_armstrong-numbers.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_armstrong-numbers"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_armstrong-numbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
