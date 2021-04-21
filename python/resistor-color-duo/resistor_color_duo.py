b1393f6b05b96aecedc90539ffff9873a6b0cac4
Budmin <garvin.drew@gmail.com>
2021-04-21 11:52:04 -0400

    4/21/2021

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,16 @@
+
+color_list = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+def value(colors):
+    return int(str(color_list.index(colors[0])) + str(color_list.index(colors[1])))
